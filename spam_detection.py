5013cac7aedc900fed0a7fb6ae689cd1d5d0c490
Pritiksha20 <shapritik20@gmail.com>
2025-03-10 21:14:01 +0530

    edit

--- spam_detection.py
+++ spam_detection.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
 
-# Load Dataset
 data = pd.read_csv('spam.csv', encoding='latin-1')
 data = data[['label', 'message']]
 data.columns = ['label', 'message']
